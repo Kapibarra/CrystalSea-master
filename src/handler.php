e91de1f47250b3174656b91fc28d23ed65e52d17
kapibarra2315 <nikita.nov2016@yandex.ru>
2023-03-07 17:47:18 +0300

    crystal sea working tree

---
+++ src/handler.php
@@ -0,0 +1,57 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'php/PHPMailer-5.2.28/src/Exception.php';
+require 'php/PHPMailer-5.2.28/src/PHPMailer.php';
+require 'php/PHPMailer-5.2.28/src/SMTP.php';
+
+$mail = new PHPMailer(true);
+$mail_subject = 'Заявка с сайта';
+$mail_to_email = 'nikita.nov2016@yandex.ru'; // your email
+$mail_to_name = 'novikovweb.ru';
+
+try {
+
+	$mail_from_email = isset( $_POST['email'] ) ? $_POST['email'] : '';
+	$mail_from_name = isset( $_POST['name'] ) ? $_POST['name'] : '';
+	$mail_category = isset( $_POST['category'] ) ? $_POST['category'] : '';
+	$mail_city = isset( $_POST['city'] ) ? $_POST['city'] : '';
+	$mail_phone = isset( $_POST['phone'] ) ? $_POST['phone'] : '';
+
+	// Server settings
+	$mail->isSMTP(); // Send using SMTP
+	$mail->Host = 'smtp.yandex.com'; // Set the SMTP server to send through
+	$mail->SMTPAuth = true; // Enable SMTP authentication
+	$mail->Username = 'noviweb@yandex.ru'; // SMTP username
+	$mail->Password = 'cf911313'; // SMTP password
+	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+	$mail->Port = 465; // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+	$mail->setFrom($mail->Username, 'Aquaroom.ru'); // Your email
+	$mail->addAddress($mail_to_email, $mail_to_name); // Add a recipient на какой емейл отправить!! проверить!!!!
+
+	for($ct=0; $ct<count($_FILES['file_attach']['tmp_name']); $ct++) {
+		$mail->AddAttachment($_FILES['file_attach']['tmp_name'][$ct], $_FILES['file_attach']['name'][$ct]);
+	}
+
+	// Content
+	$mail->isHTML(true); // Set email format to HTML
+
+	$mail->Subject = $mail_subject;
+	$mail->Body = '
+	<strong>City:</strong> ' . $mail_city . '<br>
+		<strong>Category:</strong> ' . $mail_category . '<br>
+		<strong>Name:</strong> ' . $mail_from_name . '<br>
+		<strong>Email:</strong> ' . $mail_from_email . '<br>
+		<strong>Phone:</strong> ' . $mail_phone;
+	$mail->Send();
+
+	echo 'Message has been sent';
+
+} catch (Exception $e) {
+
+	echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+
+}
---
+++ webpack.config.js
@@ -0,0 +1,113 @@
+const path = require("path");
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+// const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isProd = process.env.NODE_ENV === "production";
+const isDev = !isProd;
+
+const filename = (ext) => (isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`);
+
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+        plugins: ["@babel/plugin-proposal-class-properties"],
+      },
+    },
+  ];
+  return loaders;
+};
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: ["@babel/polyfill", "./index.js"],
+  output: {
+    filename: filename("js"),
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".js"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@core": path.resolve(__dirname, "src/core"),
+    },
+  },
+  devtool: isDev ? "source-map" : false,
+  devServer: {
+    port: 3000,
+    hot: isDev,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      filename: "index.html",
+      template: "index.html",
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename("css"),
+    }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: "url-loader",
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(sass|less|css)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: jsLoaders(),
+      },
+    ],
+  },
+};
